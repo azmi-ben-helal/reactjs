0d3d6f360eff773307b4ccc9a2c9289ac94fae8b
azmi ben helal <abenhelal@ats-digital.com>
2020-11-20 11:29:51 +0100

    cleanup && add video.mp4

--- jsxproject/src/App.js
+++ jsxproject/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import './css/style.css'
+import ImageInSrc from './images/images.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ border: 'solid 1px black', maxWidth: '100vw' }}>
+      <h1 className="title red">Azmi Ben Helal</h1>
+
+      <br />
+
+      <img src={ImageInSrc} width="320" height="240" />
+
+      <br />
+
+      <img src="./images/publicImage.jpeg" width="320" height="240" />
+
+      <br />
+      <video width="320" height="240" controls>
+        <source
+          src="./videos/two-steps-from-hell-wastelands.mp4"
+          type="video/mp4"
+        />
+      </video>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
